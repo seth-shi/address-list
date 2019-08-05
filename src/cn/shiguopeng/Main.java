772d052d3ecea3b04be78b0ebe7c9ea47dc08003
david <1033404553@qq.com>
2019-08-05 22:05:59 +0800

    初始化项目类

---
+++ src/cn/shiguopeng/Main.java
@@ -0,0 +1,12 @@
+package cn.shiguopeng;
+
+import cn.shiguopeng.views.Login;
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Application.launch(Login.class, args);
+    }
+}
---
+++ src/cn/shiguopeng/views/Login.java
@@ -0,0 +1,19 @@
+package cn.shiguopeng.views;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.setWidth(600);
+        stage.setHeight(400);
+
+        // 用户名和密码
+
+
+        stage.show();
+    }
+}
